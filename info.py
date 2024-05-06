fd3b9aed8ed8b46faec450978aa83cc6e645a8eb
Sαɳԃιρ <102131200+Sandip10bot@users.noreply.github.com>
2024-05-06 16:24:32 +0530

    Update info.py

--- info.py
+++ info.py
@@ -89,10 +89,10 @@ MELCOW_NEW_USERS = is_enabled((environ.get('MELCOW_NEW_USERS', "True")), True)
 PROTECT_CONTENT = is_enabled((environ.get('PROTECT_CONTENT', "False")), False)
 PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
-USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
+USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
 # Shortlink Info
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'krishnalink.com')
+SHORTFalURL = environ.get('SHORTLINK_URL', 'krishnalink.com')
 SHORTLINK_API = environ.get('SHORTLINK_API', '00da194554d8396caed7e76ddb6a27cd99cb1fd0')
 
 # Others
