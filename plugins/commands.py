1dc9059278ae4cd822bb43b576d1198aaae0bb49
• Kŕrish <83119148+HKrrish@users.noreply.github.com>
2024-05-07 16:48:21 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1492,7 +1492,50 @@ async def check_plans_cmd(client, message):
         await message.reply_text(f"**😢 You Don't Have Any Premium Subscription.\n\n Check Out Our Premium /plan**",reply_markup=reply_markup)
         await asyncio.sleep(2)
         await m.delete()
+
+
 @Client.on_message(filters.command("serials"))
+async def serials(bot, message):
+    msg = await bot.send_message(text="**Processing...**", chat_id=message.chat.id)       
+    await asyncio.sleep(2)
+    buttons = [[
+        InlineKeyboardButton('1988', callback_data='1988'),
+        InlineKeyboardButton('1993', callback_data='1993'),
+        InlineKeyboardButton('1997', callback_data='1997'),
+        InlineKeyboardButton('1998', callback_data='1998')
+    ],[
+        InlineKeyboardButton('2000', callback_data='2000'),
+        InlineKeyboardButton('2002', callback_data='2002'),
+        InlineKeyboardButton('2008', callback_data='2008'),
+        InlineKeyboardButton('2009', callback_data='2009')
+    ],[
+        InlineKeyboardButton('2011', callback_data='2011'),
+        InlineKeyboardButton('2013', callback_data='2013'),
+        InlineKeyboardButton('2015', callback_data='2015'),
+        InlineKeyboardButton('2017', callback_data='2017')
+    ],[
+        InlineKeyboardButton('2018', callback_data='2018'),
+        InlineKeyboardButton('2019', callback_data='2019'),
+        InlineKeyboardButton('2020', callback_data='2020'),
+        InlineKeyboardButton('2021', callback_data='2021')
+    ],[
+        InlineKeyboardButton('2022', callback_data='2022'),
+        InlineKeyboardButton('2023', callback_data='2023'),
+        InlineKeyboardButton('2024', callback_data='2024')
+        #InlineKeyboardButton('1998', callback_data='1998')
+    ],[
+        InlineKeyboardButton('🔥 Updates!', url='https://t.me/Radhakriishn')
+    ]]
+    reply_markup = InlineKeyboardMarkup(buttons)
+    await message.reply_photo(
+            photo=random.choice(PICS),
+            caption=script.SERIALS_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
+            reply_markup=reply_markup,
+            parse_mode=enums.ParseMode.HTML
+    )
+    await msg.delete()
+
+#@Client.on_message(filters.command("serials"))
 async def availableserial(bot, message):
     await message.reply_text(SERIALS)     
  
